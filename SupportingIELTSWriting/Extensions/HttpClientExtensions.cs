bb83c6c180e8e16c2f1afd6a0760df90dfb82bbf
Luong Tran <35062710+Luongtran99@users.noreply.github.com>
2021-05-01 21:24:17 +0700

    add extensions
    Add auth extension and session extension

---
+++ SupportingIELTSWriting/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportingIELTSWriting.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static void SetBasicAuthentication(this HttpClient client, string username, string password) => client.DefaultRequestHeaders.Authorization = new BasicAuthenticationHeaderValue(username, password);
+        public static void SetToken(this HttpClient client, string scheme, string token) => client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(scheme, token);
+        public static void SetBearerToken(this HttpClient client, string token) => client.SetToken(JwtConstants.TokenType, token);
+    }
+
+    public class BasicAuthenticationHeaderValue : System.Net.Http.Headers.AuthenticationHeaderValue
+    {
+        public BasicAuthenticationHeaderValue(string username, string password) : base("Basic", EncodeCredential(username,password))
+        {
+
+        }
+
+        private static string EncodeCredential(string username, string password)
+        {
+            Encoding encoding = Encoding.GetEncoding("iso-8859-1");
+            string credential = String.Format("{0}:{1}", username, password);
+
+            return Convert.ToBase64String(encoding.GetBytes(credential));
+        }
+    }
+}
