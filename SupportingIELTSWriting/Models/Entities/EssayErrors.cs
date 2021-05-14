019def7c68497efbf1840e03831e62d8e5ab5925
Luong Tran <35062710+Luongtran99@users.noreply.github.com>
2021-05-14 22:33:12 +0700

    add EssayError
    Error Description

---
+++ SupportingIELTSWriting/Models/Entities/EssayErrors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupportingIELTSWriting.Models.Entities
+{
+    public class EssayErrors
+    {
+        public string Message { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+        public List<string> Replacements { get; set; }
+
+    }
+}
